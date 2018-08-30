510e50e31ad7bac52c1200bea0946f0b575fc5b3
Spencer Casteel <spencercasteel@interapts-macbook-pro-9.local>
2018-08-30 10:47:33 -0500

    Initial Commit

---
+++ VideoGameLibrary/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  VideoGameLibrary
+//
+//  Created by Spencer Casteel on 8/30/18.
+//  Copyright © 2018 Spencer Casteel. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
